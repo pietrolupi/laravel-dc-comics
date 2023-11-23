5ed4991773077c417c0d45078d29345765087a96
pietrolupi <pietro.lupi92@gmail.com>
2023-11-23 16:54:06 +0100

    [add] tentativo seed

---
+++ database/migrations/2023_11_23_145606_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('slug',100)->unique();
+            $table->text('description')->nullable();
+            $table->string('thumb');
+            $table->string('price');
+            $table->string('series',50);
+            $table->date('sale_date');
+            $table->string('type',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+use Illuminate\Support\Str;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(){
+
+    $comics = config('comics');
+
+    foreach ($comics as $comic) {
+
+        $new_comic = new Comic();
+
+        $new_comic->title = $comic['title'];
+        $new_comic->slug = $this->generateSlug($new_comic->title);
+        $new_comic->description = $comic['description'];
+        $new_comic->thumb = $comic['thumb'];
+        $new_comic->price = $comic['price'];
+        $new_comic->series = $comic['series'];
+        $new_comic->sale_date = $comic['sale_date'];
+        $new_comic->type = $comic['type'];
+
+        $new_comic->save();
+    }
+}
+
+    private function generateSlug($string){
+        $slug = Str::slug($string, '-');
+        $original_slug = $slug;
+
+        $exists = Comic::where('slug', $slug)->first();
+        $c = 1;
+
+        while($exists){
+            $slug = $original_slug. '-'. $c;
+            $exists = Comic::where('slug', $slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
+}
